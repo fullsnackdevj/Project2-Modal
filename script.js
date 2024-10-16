e8d79a58449483bd7390b1e1acb675d0312361d4
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-10-16 19:14:22 +0800

    added working with classes

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+// / function opening the modal
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// function closing the modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 // selecting multiple elements with the same class
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// adding eventhandler on elements
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//response
