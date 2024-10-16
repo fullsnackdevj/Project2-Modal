1ba234e7c9c412356f95548e9ea6c002871a6e16
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-10-16 13:54:44 +0800

    introduction to project 2 modal

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 'use strict';
 
+//selecting element classes
+
 const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('.overlay');
+
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+// selecting multiple elements with the same class
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
